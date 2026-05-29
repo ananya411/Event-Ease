a540d8e88bd827d2fbf67c4c3f853c5a48494410
Ananya Jaiswal <ananyasbr06@gmail.com>
2026-05-29 13:59:19 +0530

    fix entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,7 +6,11 @@ php artisan route:clear
 php artisan view:clear
 
 php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# Database migrate (MongoDB ke liye bhi kaam karta hai)
+php artisan migrate --force
 
 php-fpm &
 nginx -g "daemon off;"
-
